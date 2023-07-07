afb832e0d4a830ae8b39935dfc3c441c016fba8b
k226470 <konstantin.roeder@kcl.ac.uk>
2023-07-07 12:28:33 +0100

    Added biological constraints to QCI detection

--- source/mod_amber.f90
+++ source/mod_amber.f90
@@ -12,8 +12,10 @@ MODULE AMBER_CONSTRAINTS
    CHARACTER(LEN=25) :: TOPFILENAME = "coords.prmtop"
    INTEGER, ALLOCATABLE :: BONDS(:,:)
    INTEGER, ALLOCATABLE :: ANGLES(:,:)
+   INTEGER, ALLOCATABLE :: BIOCONSTR(:,:)   
    INTEGER :: NBOND = 0
    INTEGER :: NANGLE = 0
+   INTEGER :: NBIOCONSTR = 0 ! biological constraints - cis-trans and planarity
    INTEGER, ALLOCATABLE :: RESFINAL
    CHARACTER(LEN=4), ALLOCATABLE :: AMBER_NAMES(:), RESNAMES(:)
 
@@ -30,7 +32,7 @@ MODULE AMBER_CONSTRAINTS
          LOGICAL :: YESNO ! do we have a contact file
          INTEGER :: CONUNIT ! unit for opening file
          INTEGER :: NADDCONSTR ! number of additional constraints
-         INTEGER :: NBIOCONSTR ! biological constraints - cis-trans and planarity 
+ 
          INTEGER :: IDX1, IDX2 !indices for atoms
 
          ! parse topology
@@ -42,9 +44,10 @@ MODULE AMBER_CONSTRAINTS
          ELSE
             NADDCONSTR = 0
          END IF
+         CALL GET_BIOCONSTR()
 
          ! allocate arrays
-         AMBER_NCONST = NBOND + NANGLE + NADDCONSTR
+         AMBER_NCONST = NBOND + NANGLE + NBIOCONSTR + NADDCONSTR
          ! allocate the required arrays
          CALL ALLOC_AMBER_CONSTR()
          NDUMMY = 0
@@ -82,6 +85,24 @@ MODULE AMBER_CONSTRAINTS
             AMBER_CONDISTREF(NDUMMY) = (DF+DS)/2.0D0
             AMBER_CONCUT(NDUMMY) = ABS(DF-DS)/2.0D0
          END DO
+         ! add bioconstraints
+         DO J1=1,NBIOCONSTR
+            NDUMMY = NDUMMY + 1
+            IDX1 = BIOCONSTR(J1,1)
+            IDX2 = BIOCONSTR(J1,2)
+            CALL DISTANCE_TWOATOMS(NATOMS, XSTART, IDX1, IDX2, DS)
+            CALL DISTANCE_TWOATOMS(NATOMS, XFINAL, IDX1, IDX2, DF)
+            IF (IDX1.LT.IDX2) THEN
+               AMBER_CONI(NDUMMY) = IDX1
+               AMBER_CONJ(NDUMMY) = IDX2
+            ELSE
+               AMBER_CONI(NDUMMY) = IDX2
+               AMBER_CONJ(NDUMMY) = IDX1
+            END IF
+            AMBER_CONDISTREF(NDUMMY) = (DF+DS)/2.0D0
+            AMBER_CONCUT(NDUMMY) = ABS(DF-DS)/2.0D0
+         END DO
+
          ! now add additional constraints from file provided
          IF (YESNO) THEN
             CONUNIT = GETUNIT()
@@ -117,7 +138,7 @@ MODULE AMBER_CONSTRAINTS
          CHARACTER(LEN=4) :: ATNAME
          INTEGER :: J1
 
-         NDUMMY = 0
+         NBIOCONSTR = 0
          DO J1=1,NATOMS
             ATNAME = ADJUSTL(TRIM(AMBER_NAMES(J1)))
             IF ((ATNAME.EQ.'C').OR.(ATNAME.EQ.'O').OR.(ATNAME.EQ.'N').OR.(ATNAME.EQ.'H').OR.(ATNAME.EQ.'CA') &
@@ -135,8 +156,229 @@ MODULE AMBER_CONSTRAINTS
          BACKBONE(1:NBACKBONE) = DUMMY_BB(1:NBACKBONE)
       END SUBROUTINE GET_BACKBONE
 
+      SUBROUTINE GET_BIOCONSTR()
+         IMPLICIT NONE
+        
+         INTEGER :: NDUMMY, J1, OPOS1, CPOS1, CPOS2, HPOS2
+         LOGICAL :: AAT, RNAT, DNAT, CPAT, AALIST(NRES), ISTER(NRES), ISCAP(NRES)
+         INTEGER, ALLOCATABLE :: DUMMYC(:,:)
 
 
+         ! the current largest number of additional constraints per residue is 8 and we have two additional constraints for peptide bonds,
+         ! and seven constraints for chiral centres and sugar and base in nucleic acids - the worst case seems like 16 constraints per residue
+         ! we use 20 for this array size, and should have plenty of space
+         ALLOCATE(BIOCONSTR(20*NRES,2))
+         BIOCONSTR(:,:) = -1
+         NDUMMY = 0
+         AALIST(1:NRES) = .FALSE.
+         ISTER(1:NRES) = .FALSE.
+         ISCAP(1:NRES) = .FALSE.
+
+         ! this is the first round to add constraints for planarity, the sugars in NAs etc.
+         DO J1=1,NRES
+            !find what kind of residue we have
+            CALL CHECK_RES(J1, AAT, DNAT, RNAT)
+            IF (CPAT) THEN
+               ISCAP(J1) = .TRUE.
+               AALIST(J1) = .TRUE.
+            ! for amino acids check whether this is a temrinal residue and then find specific constraints
+            ELSE IF (AAT) THEN
+               CALL GET_ATOMID("OXT",J1,ATOMID)
+               IF (ATOMID.NE.0) THEN
+                  ISTER(J1) = .TRUE.
+               ELSE
+                  CALL GET_ATOMID("H1",J1,ATOMID)
+                  IF (ATOMID.NE.0) THEN
+                     ISTER(J1) = .TRUE.
+                  END IF
+               END IF
+               AALIST(J1) = .TRUE.
+               CALL GET_AA_CONSTR(J1)
+            ELSE IF (RNAT.OR.DNAT) THEN
+               CALL GET_NA_CONSTR(J1)
+            END IF
+         END DO
+
+         ! peptide bonds
+         DO J1=1,NRES-1
+            IF (AALIST(J1)) THEN
+               ! chekc we are not looking at capping groups
+               IF ((.NOT.ISCAP(J1)).AND.(.NOT.ISCAP(J1+1))) THEN
+                  IF (.NOT.ISTER(J1)) THEN
+                     CALL GET_ATOMID("O",J1,OPOS1)
+                     CALL GET_ATOMID("CA",J1,CPOS1)
+                     CALL GET_ATOMID("H",J1+1,HPOS2)
+                     CALL GET_ATOMID("CA",J1+1,HPOS2)
+                     ! O(i) - H(i+1)
+                     IF ((OPOS1.GT.0).AND.(HPOS2.GT.0)) THEN
+                        NBIOCONSTR = NBIOCONSTR + 1
+                        BIOCONSTR(NBIOCONSTR,1) = OPOS1
+                        BIOCONSTR(NBIOCONSTR,2) = HPOS2
+                     END IF
+                     ! CA(i) - CA(i+1)
+                     IF ((CPOS1.GT.0).AND.(CPOS2.GT.0)) THEN
+                        NBIOCONSTR = NBIOCONSTR + 1
+                        BIOCONSTR(NBIOCONSTR,1) = CPOS1
+                        BIOCONSTR(NBIOCONSTR,2) = CPOS2
+                     END IF
+                     ! CA(i) - H(i+1)
+                     IF ((CPOS1.GT.0).AND.(HPOS2.GT.0)) THEN
+                        NBIOCONSTR = NBIOCONSTR + 1
+                        BIOCONSTR(NBIOCONSTR,1) = CPOS1
+                        BIOCONSTR(NBIOCONSTR,2) = HPOS2
+                     END IF
+                  END IF
+               END IF
+            END IF
+         END DO
+
+         ALLOCATE(DUMMYC(NBIOCONSTR,2))
+         DUMMYC(1:NBIOCONSTR,1:2) = BIOCONSTR(1:NBIOCONSTR,1:2)
+         DEALLOCATE(BIOCONSTR)
+         ALLOCATE(BIOCONSTR(NBIOCONSTR,2))
+         BIOCONSTR(1:NBIOCONSTR,1:2) = DUMMYC(1:NBIOCONSTR,1:2)
+
+      END SUBROUTINE GET_BIOCONSTR
+
+      SUBROUTINE GET_AA_CONSTR(RESID)
+         IMPLICIT NONE
+         INTEGER, INTENT(IN) :: RESID
+
+         CHARACTER(LEN=4) :: NAME
+
+         NAME = RESNAMES(RESID)
+         IF ((RES.EQ.'ARG').OR.(RES.EQ.'CARG').OR.(RES.EQ.'NARG')) THEN
+            CALL ADD_CONSTRAINT("HH12","HH22",RESID)
+            CALL ADD_CONSTRAINT("HH12","HH21",RESID)
+            CALL ADD_CONSTRAINT("HH11","HH22",RESID)
+            CALL ADD_CONSTRAINT("HH11","HH21",RESID)
+            CALL ADD_CONSTRAINT("HH11","NE",RESID)
+            CALL ADD_CONSTRAINT("HH12","NE",RESID)
+            CALL ADD_CONSTRAINT("HH21","NE",RESID)
+            CALL ADD_CONSTRAINT("HH22","NE",RESID)
+         ELSE IF ((RES.EQ.'HID').OR.(RES.EQ.'HIE').OR.(RES.EQ.'HIS').OR.(RES.EQ.'HIP').OR. &
+                  (RES.EQ.'CHID').OR.(RES.EQ.'CHIE').OR.(RES.EQ.'CHIS').OR.(RES.EQ.'CHIP').OR. &
+                  (RES.EQ.'NHID').OR.(RES.EQ.'NHIE').OR.(RES.EQ.'NHIS').OR.(RES.EQ.'NHIP')) THEN
+            CALL ADD_CONSTRAINT("HD1","NE2",RESID)
+            CALL ADD_CONSTRAINT("HE1","CD2",RESID)
+            CALL ADD_CONSTRAINT("HE2","CG",RESID)
+            CALL ADD_CONSTRAINT("HD2","ND1",RESID)
+         ELSE IF ((NAME.EQ.'PHE').OR.(NAME.EQ.'CPHE').OR.(NAME.EQ.'NPHE')) THEN
+            CALL ADD_CONSTRAINT("HD2","CE1",RESID)
+            CALL ADD_CONSTRAINT("HE2","CD1",RESID)
+            CALL ADD_CONSTRAINT("HZ","CG",RESID)
+            CALL ADD_CONSTRAINT("HE1","CD2",RESID)
+            CALL ADD_CONSTRAINT("HD1","CE2",RESID)
+            CALL ADD_CONSTRAINT("CB","CZ",RESID)
+         ELSE IF ((RES.EQ.'TRP').OR.(RES.EQ.'CTRP').OR.(RES.EQ.'NTRP')) THEN
+            CALL ADD_CONSTRAINT("HD1","CD2",RESID)
+            CALL ADD_CONSTRAINT("HE1","CG",RESID)
+            CALL ADD_CONSTRAINT("HZ2","CE3",RESID)
+            CALL ADD_CONSTRAINT("HH2","CD2",RESID)
+            CALL ADD_CONSTRAINT("HZ3","CE2",RESID)
+            CALL ADD_CONSTRAINT("HE3","CZ2",RESID)
+         ELSE IF ((RES.EQ.'TYR').OR.(RES.EQ.'CTYR').OR.(RES.EQ.'NTYR')) THEN
+            CALL ADD_CONSTRAINT("HD1","CE2",RESID)
+            CALL ADD_CONSTRAINT("HE1","CD2",RESID)
+            CALL ADD_CONSTRAINT("OH","CG",RESID)
+            CALL ADD_CONSTRAINT("HE2","CD1",RESID)
+            CALL ADD_CONSTRAINT("HD2","CE1",RESID)
+            CALL ADD_CONSTRAINT("CB","CZ",RESID)
+         END IF 
+      END SUBROUTINE GET_AA_CONSTR
+
+      SUBROUTINE GET_NA_CONSTR(RESID)
+         IMPLICIT NONE
+         INTEGER, INTENT(IN) :: RESID
+
+         CHARACTER(LEN=4) :: NAME
+
+         NAME = RESNAMES(RESID)
+         IF ((RESNAMES(J1).EQ.'A').OR.(RESNAMES(J1).EQ.'A3').OR.(RESNAMES(J1).EQ.'A5').OR.  &
+             (RESNAMES(J1).EQ.'AN').OR.(RESNAMES(J1).EQ.'DA').OR.(RESNAMES(J1).EQ.'DA3').OR.  &
+             (RESNAMES(J1).EQ.'DA5').OR.(RESNAMES(J1).EQ.'DAN')) THEN
+            
+            CALL ADD_CONSTRAINT("N1","N9",RESID)        
+            CALL ADD_CONSTRAINT("C2","N7",RESID) 
+            CALL ADD_CONSTRAINT("N3","C6",RESID) 
+            CALL ADD_CONSTRAINT("N3","N6",RESID) 
+            CALL ADD_CONSTRAINT("C4","N1",RESID) 
+            CALL ADD_CONSTRAINT("C5","H2",RESID) 
+            CALL ADD_CONSTRAINT("C8","C4",RESID) 
+            CALL ADD_CONSTRAINT("C8","C5",RESID) 
+
+         ELSE IF ((RESNAMES(J1).EQ.'C').OR.(RESNAMES(J1).EQ.'C3').OR.(RESNAMES(J1).EQ.'C5').OR.  &
+                  (RESNAMES(J1).EQ.'CN').OR.(RESNAMES(J1).EQ.'DC').OR.(RESNAMES(J1).EQ.'DC3').OR.  &
+                  (RESNAMES(J1).EQ.'DC5').OR.(RESNAMES(J1).EQ.'DCN')) THEN
+
+            CALL ADD_CONSTRAINT("N1","N4",RESID) 
+            CALL ADD_CONSTRAINT("C2","H5",RESID) 
+            CALL ADD_CONSTRAINT("N3","H6",RESID) 
+            CALL ADD_CONSTRAINT("N3","C6",RESID) 
+            CALL ADD_CONSTRAINT("C4","N1",RESID) 
+            CALL ADD_CONSTRAINT("C5","O2",RESID) 
+        
+         ELSE IF ((RESNAMES(J1).EQ.'G').OR.(RESNAMES(J1).EQ.'G3').OR.(RESNAMES(J1).EQ.'G5').OR.  &
+                  (RESNAMES(J1).EQ.'GN').OR.(RESNAMES(J1).EQ.'DG').OR.(RESNAMES(J1).EQ.'DG3').OR.  &
+                  (RESNAMES(J1).EQ.'DG5').OR.(RESNAMES(J1).EQ.'DGN')) THEN
+        
+            CALL ADD_CONSTRAINT("N1","N9",RESID)        
+            CALL ADD_CONSTRAINT("C2","N7",RESID) 
+            CALL ADD_CONSTRAINT("N3","C6",RESID) 
+            CALL ADD_CONSTRAINT("N3","O6",RESID) 
+            CALL ADD_CONSTRAINT("C4","H1",RESID) 
+            CALL ADD_CONSTRAINT("C5","N2",RESID) 
+            CALL ADD_CONSTRAINT("C8","C4",RESID) 
+            CALL ADD_CONSTRAINT("C8","C5",RESID) 
+
+         ELSE IF ((RESNAMES(J1).EQ.'U').OR.(RESNAMES(J1).EQ.'U3').OR.(RESNAMES(J1).EQ.'U5').OR.  &
+                  (RESNAMES(J1).EQ.'UN')) THEN
+
+            CALL ADD_CONSTRAINT("N1","O4",RESID) 
+            CALL ADD_CONSTRAINT("C2","H5",RESID) 
+            CALL ADD_CONSTRAINT("N3","H6",RESID) 
+            CALL ADD_CONSTRAINT("H3","C6",RESID) 
+            CALL ADD_CONSTRAINT("C4","N1",RESID) 
+            CALL ADD_CONSTRAINT("C5","O2",RESID)
+                  
+         ELSE IF ((RESNAMES(J1).EQ.'DT').OR.(RESNAMES(J1).EQ.'DT3').OR.  &
+                  (RESNAMES(J1).EQ.'DT5').OR.(RESNAMES(J1).EQ.'DTN')) THEN
+
+            CALL ADD_CONSTRAINT("N1","O4",RESID) 
+            CALL ADD_CONSTRAINT("C2","C7",RESID) 
+            CALL ADD_CONSTRAINT("N3","H6",RESID) 
+            CALL ADD_CONSTRAINT("N3","C6",RESID) 
+            CALL ADD_CONSTRAINT("C4","N1",RESID) 
+            CALL ADD_CONSTRAINT("C5","O2",RESID) 
+
+         ENDIF
+
+         !constrain the sugar to keep its shape
+         CALL ADD_CONSTRAINT("O4'","C2'",RESID) 
+         CALL ADD_CONSTRAINT("C1'","C3'",RESID)         
+
+         !constrain chiral centres
+         CALL ADD_CONSTRAINT("O2'","O3'",RESID)
+         CALL ADD_CONSTRAINT("H2''","O3'",RESID)
+         CALL ADD_CONSTRAINT("H3'","C5'",RESID)
+         CALL ADD_CONSTRAINT("H3'","H2'",RESID)
+         CALL ADD_CONSTRAINT("O3''","H4'",RESID)
+
+      END SUBROUTINE GET_NA_CONSTR
+
+      SUBROUTINE ADD_CONSTRAINT(ATNAME1,ATNAME2,RESID)
+         CHARACTER(LEN=4), INTENT(IN) :: ATNAME1, ATNAME2
+         INTEGER, INTENT(IN) :: RESID
+         INTEGER :: ID1, ID2 
+
+         CALL GET_ATOMID(ATNAME1,RESID,ID1)
+         CALL GET_ATOMID(ATNAME2,RESID,ID2)
+         IF ((ID1.GT.0).AND.(ID2.GT.0)) THEN
+            NBIOCONSTR = NBIOCONSTR + 1
+            BIOCONSTR(NBIOCONSTR,1) = ID1
+            BIOCONSTR(NBIOCONSTR,2) = ID2
+         END IF
+      END SUBROUTINE ADD_CONSTRAINT
 
       ! allocate amber constraints array
       SUBROUTINE ALLOC_AMBER_CONSTR()
@@ -171,12 +413,6 @@ MODULE AMBER_CONSTRAINTS
          INTEGER :: NLINES
          INTEGER :: J1, J2
          INTEGER, ALLOCATABLE :: INDICES(:)
-         INTEGER, PARAMETER :: NENT_NBONDH = 10 ! entries per line in the topology
-         INTEGER, PARAMETER :: NENT_NBONDA = 10 ! entries per line in the topology
-         INTEGER, PARAMETER :: NENT_NANGH = 10 ! entries per line in the topology
-         INTEGER, PARAMETER :: NENT_NANGA = 10 ! entries per line in the topology
-         INTEGER, PARAMETER :: NPERENT_BOND = 3 ! integers per bond entry
-         INTEGER, PARAMETER :: NPERENT_ANG = 4 ! integers per angle entry 
          CHARACTER(4) :: NAMES_CURR(20)
 
          !check topology exists
@@ -389,10 +625,10 @@ MODULE AMBER_CONSTRAINTS
       END SUBROUTINE PARSE_TOPOLOGY
 
         ! check if residue is an amino acid - brute force ...
-      SUBROUTINE CHECK_RES(RESID,AAT,DNAT,RNAT)
+      SUBROUTINE CHECK_RES(RESID,AAT,DNAT,RNAT,CAPT)
          IMPLICIT NONE
          INTEGER, INTENT(IN) :: RESID
-         LOGICAL, INTENT(OUT) :: AAT, DNAT, RNAT
+         LOGICAL, INTENT(OUT) :: AAT, DNAT, RNAT, CAPT
          CHARACTER(4) :: DNAME
          LOGICAL :: TERTEST
          
@@ -400,6 +636,7 @@ MODULE AMBER_CONSTRAINTS
          AAT = .FALSE.
          RNAT = .FALSE.
          DNAT = .FALSE.
+         CAPT = .FALSE.
          TERTEST = .FALSE.
 60       CONTINUE
          IF ((DNAME.EQ."ALA").OR.(DNAME.EQ."ARG").OR.(DNAME.EQ."ASH").OR.  &
@@ -423,9 +660,11 @@ MODULE AMBER_CONSTRAINTS
                   (DNAME.EQ."DG").OR.(DNAME.EQ."DG3").OR.(DNAME.EQ."DG5").OR.(DNAME.EQ."DGN").OR.  &
                   (DNAME.EQ."DT").OR.(DNAME.EQ."DT3").OR.(DNAME.EQ."DT5").OR.(DNAME.EQ."DTN")) THEN
             DNAT = .TRUE.
+         ELSE IF ((DNAME.EQ."ACE").OR.(DNAME.EQ."NHE").OR.(DNAME.EQ."NME")) THEN
+            CAPT = .TRUE. 
          ENDIF
          !make sure it is not a terminal residue (we technically should never encounter one)
-         IF (((.NOT.AAT).AND.(.NOT.TERTEST)).AND.((DNAME(1:1).EQ."N").OR.(DNAME(1:1).EQ."C"))) THEN
+         IF (((.NOT.AAT).AND.(.NOT.CAPT).AND.(.NOT.TERTEST)).AND.((DNAME(1:1).EQ."N").OR.(DNAME(1:1).EQ."C"))) THEN
             TERTEST=.TRUE.
             DNAME = DNAME(2:4)
             GOTO 60
