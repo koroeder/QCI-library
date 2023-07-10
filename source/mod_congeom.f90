876d391e2e68231ab4a876d8570411b38bdc21b9
k226470 <konstantin.roeder@kcl.ac.uk>
2023-07-10 15:30:25 +0100

    Module outline to use geometry to create constraints

---
+++ source/mod_congeom.f90
@@ -0,0 +1,18 @@
+MODULE CONGEOM
+
+   CONTAINS
+      ! create constraints from endpoints
+      SUBROUTINE CREATE_FROM_ENDPOINTS()
+
+      END SUBROUTINE CREATE_FROM_ENDPOINTS
+
+      ! create constraints from input geometries
+      SUBROUTINE CREATE_FROM_GEOMETRIES()
+
+      END SUBROUTINE CREATE_FROM_GEOMETRIES
+    
+      ! add constraints list
+      SUBROUTINE ADD_CONSTRAINT_LIST()
+
+      END SUBROUTINE ADD_CONSTRAINT_LIST
+END MODULE CONGEOM
