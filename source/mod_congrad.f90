856be061b79aaf3a1b6d2fe40c5f1a75aff2b83c
kr366 <konstantin.roeder@kcl.ac.uk>
2023-11-01 16:38:15 +0000

    More work on congrad routines

--- source/mod_congrad.f90
+++ source/mod_congrad.f90
@@ -124,16 +124,23 @@ MODULE CONSTR_E_GRAD
 
       SUBROUTINE GET_REPULSION_E(XYZ,GGG,EEE,EREP)
          USE QCIKEYS, ONLY: NIMAGE, NATOMS
-         USE REPULSION, ONLY: 
+         USE REPULSION, ONLY: NNREPULSIVE, NREPI, NREPJ, NREPCUT
          USE HELPER_FNCTS, ONLY: DISTANCE_SIMPLE
          IMPLICIT NONE
          REAL(KIND = REAL64), INTENT(IN) :: XYZ(3*NATOMS*(NIMAGE+2))   ! input coordinates
          REAL(KIND = REAL64), INTENT(OUT) :: GGG(3*NATOMS*(NIMAGE+2))  ! gradient for each atom in each image
          REAL(KIND = REAL64), INTENT(OUT) :: EEE(NIMAGE+2), EREP       ! energy for repulsions
          
-         INTEGER :: J1, OFFSET1, OFFSET2
-         REAL(KIND=REAL64) :: X1(3*NATOMS), X2(3*NATOMS)           ! coordinates for image 1 and 2
-
+         INTEGER :: J1, J2, OFFSET1, OFFSET2
+         REAL(KIND = REAL64) :: X1(3*NATOMS), X2(3*NATOMS)           ! coordinates for image 1 and 2
+         REAL(KIND = REAL64) :: GLOCAL1(3*NATOMS), GLOCAL2(3*NATOMS) ! gradients for each image
+         REAL(KIND = REAL64) :: EREP1, EREP2                         ! repulsion energy contributions
+         INTEGER :: NI1, NI2, NJ1, NJ2                               ! indices for atom I and J in images 1 and 2
+         REAL(KIND = REAL64) :: G1(3), G2(3), DSQ1, DSQ2             ! dummy variables
+         REAL(KIND = REAL64) :: DCUT, DINTMIN, DP_G12                ! values used to test for internal minimum
+         REAL(KIND = REAL64) , PARAMETER :: DINTTEST = 1.0D-50       ! cutoff for DINTMIN to test for internal min
+         REAL(KIND = REAL64) :: D1, D2                               ! distances in image 1 and 2
+         LOGICAL :: NOINT                                            ! no internal minimum
 
          DO J1=2,NIMAGE+1
             ! get coordinates for images
@@ -141,6 +148,43 @@ MODULE CONSTR_E_GRAD
             OFFSET1=(3*NATOMS)*(J1-2)
             X1(1:3*NATOMS)=XYZ(OFFSET1+1:OFFSET1+3*NATOMS)
             X2(1:3*NATOMS)=XYZ(OFFSET2+1:OFFSET2+3*NATOMS) 
+            ! initialise some local vairables
+            GLOCAL1(1:3*NATOMS) = 0.0D0; GLOCAL2(1:3*NATOMS) = 0.0D0
+            EREP1 = 0.0D0; EREP2 = 0.0D0
+            ! iterate over all repulsions
+            DO J2 = 1,NNREPULSIVE
+               NI1=3*(NREPI(J2)-1)
+               NI2=3*(NREPI(J2)-1)
+               NJ1=3*(NREPJ(J2)-1)
+               NJ2=3*(NREPJ(J2)-1)               
+               G1(1:3)=X1(NI1+1:NI1+3)-X1(NJ1+1:NJ1+3) !vector from j to i in image 1
+               G2(1:3)=X2(NI2+1:NI2+3)-X2(NJ2+1:NJ2+3) !vector from j to i in image 2
+               ! squared distance between atoms in image 1 (theta = pi/2)
+               DSQ1=G1(1)**2 + G1(2)**2 + G1(3)**2
+               ! squared distance between atoms in image 2 (theta = 0)
+               DSQ2=G2(1)**2 + G2(2)**2 + G2(3)**2
+               DCUT = NREPCUT(J2)**2
+               ! don't look for an internal minimum if both repulsions outside cutoff
+               IF ((DSQ1.GT.DCUT).AND.(DSQ2.GT.DCUT)) CYCLE
+               ! don't check for internal minimum in distance - atoms too close for chain crossing.
+               IF (ABS(NREPI(J2)-NREPJ(J2)).LT.QCIINTREPMINSEP) THEN
+                  DINTMIN = 0.0D0
+               ELSE 
+                  DP_G12 = DOT_PRODUCT(G1,G2)
+                  DINTMIN = DSQ1+DSQ2-2.0D0*DP_G12
+               END IF
+
+               ! if the denominator in the d^2 is approx zero, we do not need to check for an internal minimum
+               IF (DINTMIN.LT.DINTTEST) THEN
+                  NOINT = .TRUE.
+                  D1 = SQRT(DSQ1); D2 = SQRT(DSQ1)
+                  G1(1:3) = G1(1:3)/D1; G2(1:3) = G2(1:3)/D2
+               ELSE
+                  !TODO: write MINMAX routine
+                  CALL MINMAXD2R()
+               END IF
+               ! TODO: continue line 242
+            END DO 
          END DO
 
 
