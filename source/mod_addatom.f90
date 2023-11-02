6f11f4d39076beea071f104e7de92dd016d90629
k226470 <konstantin.roeder@kcl.ac.uk>
2023-11-02 16:42:19 +0000

    New routines started for adding atoms

--- source/mod_addatom.f90
+++ source/mod_addatom.f90
@@ -1,13 +1,30 @@
 MODULE ADDATOM
    USE QCIPREC
+   IMPLICIT NONE
+   LOGICAL :: BBDONE = .FALSE.
 
    CONTAINS
 
       SUBROUTINE ADDNEXTATOM()
-
+         USE QCIKEYS, ONLY: QCIDOBACK
+         IMPLICIT NONE
+         
+         IF (QCIDOBACK.AND.(.NOT.BBDONE)) CALL CHECK_BBLIST()
 
       END SUBROUTINE ADDNEXTATOM
 
+      SUBROUTINE CHECK_BBLIST()
+         USE QCIKEYS, ONLY: NATOMS
+         DO J1=1,NATOMS
+            IF (AABACK(J1)) THEN
+               IF (.NOT.ATOMACTIVE(J1)) THEN
+                  ! if there are inactive BB atoms, return
+                  RETURN
+               END IF
+            ENDIF
+         ENDDO
+         BBDONE = .TRUE.
+      END SUBROUTINE CHECK_BBLIST
 
 
 END MODULE ADDATOM
